585fabc2b7e075ce2d4325f42403deb27cafe2c6
Fernando Ereno <erenof79@gmail.com>
2022-03-31 00:14:56 -0300

    Initial commint with empty spring boot project

---
+++ src/main/java/com/tinderpets/TinderpetsApplication.java
@@ -0,0 +1,13 @@
+package com.tinderpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderpetsApplication.class, args);
+	}
+
+}
